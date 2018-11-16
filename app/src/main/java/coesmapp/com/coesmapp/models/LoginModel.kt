3f7d0a941af662dbe2fe56f753d972c5dab8a7b5
Trust Mubaiwa <trustmub@gmail.com>
2018-11-16 03:56:05 +0200

    architecture phase 2: Configured the Dipendency injection usind Koin

--- app/src/main/java/coesmapp/com/coesmapp/models/LoginModel.kt
+++ app/src/main/java/coesmapp/com/coesmapp/models/LoginModel.kt
@@ -1,18 +1,21 @@
 package coesmapp.com.coesmapp.models
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 
 data class LoginModel(val username: String, val password: String)
 
 enum class Gender { MALE, FEMALE }
 
-@Entity
+@Entity(tableName = "user_details")
 data class User(
-    val propertyOwner: Boolean,
-    val firstName: String,
-    val surname: String,
-    val gender: Gender,
-    val identificationNumber: String,
-    val email: String,
-    val phone: Int
+    @ColumnInfo(name = "property_owner") val propertyOwner: Boolean,
+    @ColumnInfo(name = "first_name") val firstName: String,
+    @ColumnInfo(name = "surname") val surname: String,
+    @ColumnInfo(name = "gender") val gender: String,
+    @ColumnInfo(name = "identification_number") val identificationNumber: String,
+    @ColumnInfo(name = "email") val email: String,
+    @ColumnInfo(name = "phone") val phone: Int,
+    @PrimaryKey(autoGenerate = false) val id: Int = 0
 )
--- app/src/main/java/coesmapp/com/coesmapp/utilities/ApplicationDatabase.kt
+++ app/src/main/java/coesmapp/com/coesmapp/utilities/ApplicationDatabase.kt
@@ -3,10 +3,11 @@ package coesmapp.com.coesmapp.utilities;
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import coesmapp.com.coesmapp.models.LoginModel
+import coesmapp.com.coesmapp.models.User
 import coesmapp.com.coesmapp.models.room.LoginDao
 
 
-@Database(entities = arrayOf(LoginModel::class), version = 1, exportSchema = true)
+@Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): LoginDao
