deb603519aaea7b673c4ea3f36abc4802b1fdaeb
Trust Mubaiwa <trustmub@gmail.com>
2018-12-13 01:35:40 +0200

    capture image and save to device

--- app/src/main/java/coesmapp/com/coesmapp/ui/home/CaptureReadingFragment.kt
+++ app/src/main/java/coesmapp/com/coesmapp/ui/home/CaptureReadingFragment.kt
@@ -1,11 +1,15 @@
 package coesmapp.com.coesmapp.ui.home
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,12 +20,15 @@ import coesmapp.com.coesmapp.utilities.DialogUtil
 import coesmapp.com.coesmapp.utilities.setupToolbarAndTitle
 import kotlinx.android.synthetic.main.fragment_capture_reading.*
 import kotlinx.android.synthetic.main.fragment_capture_reading.view.*
-
-private const val  REQUEST_IMAGE_CAPTURE = 1
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CaptureReadingFragment : BaseFragment() {
 
-
+    private lateinit var currentPhotoPath: String
+    private val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_capture_reading, container, false)
@@ -29,12 +36,8 @@ class CaptureReadingFragment : BaseFragment() {
         activity!!.setupToolbarAndTitle(R.id.toolbar_home, "Capture meter reading")
 
         view.img_capture_reading.setOnClickListener {
-            println("Capture picture clicked")
             dispatchTakePictureIntent()
         }
-
-
-
 
         view.btn_submit_reading.setOnClickListener {
             // save the captured information in the database/or network. when successful display the dialog
@@ -53,20 +56,45 @@ class CaptureReadingFragment : BaseFragment() {
         return view
     }
 
-
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             takePictureIntent.resolveActivity(activity!!.packageManager)?.also {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                photoFile?.also { file ->
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        activity!!,
+                        "coesmapp.com.coesmapp.fileprovider",
+                        file
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                }
             }
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             val imageBitmap = data?.extras!!.get("data") as Bitmap
             img_capture_reading.setImageBitmap(imageBitmap)
         }
     }
+
+    @Throws(IOException::class)
+    @SuppressLint("SimpleDateFormat")
+    private fun createImageFile(): File {
+
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = activity!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile("JPEG_${timestamp}_", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
 }
--- app/src/main/java/coesmapp/com/coesmapp/ui/home/MeterReadingFragment.kt
+++ app/src/main/java/coesmapp/com/coesmapp/ui/home/MeterReadingFragment.kt
@@ -1,8 +1,5 @@
 package coesmapp.com.coesmapp.ui.home
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +7,6 @@ import android.view.ViewGroup
 import coesmapp.com.coesmapp.R
 import coesmapp.com.coesmapp.ui.common.BaseFragment
 import kotlinx.android.synthetic.main.activity_home.view.*
-import kotlinx.android.synthetic.main.fragment_capture_reading.view.*
-import android.support.v4.content.FileProvider
-import android.provider.MediaStore
-import android.util.Log
-import coesmapp.com.coesmapp.BuildConfig
-import java.io.File
-import java.io.IOException
 
 
 class MeterReadingFragment : BaseFragment() {
