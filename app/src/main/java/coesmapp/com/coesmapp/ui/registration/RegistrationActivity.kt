d3cd93f2483630113e36802d53a91a6e0ac29373
Trust Mubaiwa <trustmub@gmail.com>
2018-12-13 02:13:43 +0200

    Theming the App

--- app/src/main/java/coesmapp/com/coesmapp/ui/registration/RegistrationActivity.kt
+++ app/src/main/java/coesmapp/com/coesmapp/ui/registration/RegistrationActivity.kt
@@ -1,6 +1,5 @@
 package coesmapp.com.coesmapp.ui.registration
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import coesmapp.com.coesmapp.R
 import coesmapp.com.coesmapp.ui.common.BaseActivity
